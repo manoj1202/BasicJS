a8d0535c766c1a5f96b8c17323fe5d5a20d61de1
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-27 15:09:35 -0500

    only one file to git project

---
+++ Boolean.js
@@ -0,0 +1,16 @@
+/* let age = 43;
+let child = age <= 7;
+let senior = age >= 65;
+console.log(child);
+console.log(senior);
+*/
+
+// with if statement
+let age = 66;
+
+if(age <= 7){
+    console.log('You will get child Discount! \n ')
+}
+if (age >=65){
+    console.log('you will get senior citizen Discount! \n')
+}
