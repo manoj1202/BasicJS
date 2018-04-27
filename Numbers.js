db7b9f27812b14689294f45b06a7a785850990a3
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-27 15:18:57 -0500

    init brach commit

---
+++ Numbers.js
@@ -0,0 +1,6 @@
+
+//challenge
+let studentScore = 45;
+let maxScore = 50;
+let percentage = (studentScore / maxScore) * 100;
+console.log(percentage + '%');
---
+++ String.js
@@ -0,0 +1,5 @@
+let city="Kansas";
+let country="United states";
+let location=city+','+' '+country;
+console.log(location);
+
